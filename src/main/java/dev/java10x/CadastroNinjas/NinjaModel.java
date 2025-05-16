3d67201bb6eb55ef17118e6b97a0164c80a9b047
egplzr <egpelizaro@gmail.com>
2025-05-16 11:25:11 -0300

    Entidade Ninja criada

---
+++ src/main/java/dev/java10x/CadastroNinjas/NinjaModel.java
@@ -0,0 +1,56 @@
+package dev.java10x.CadastroNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public Long getIdTeste() {
+        return idTeste;
+    }
+
+    public void setIdTeste(Long idTeste) {
+        this.idTeste = idTeste;
+    }
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
